907c28315eb205bede3f38436e3287d9f28417d4
OmarAlHamawi <110392397+OmarAlHamawi@users.noreply.github.com>
2025-02-03 10:17:36 +0300

    Update search.cy.ts

--- users-managment-app/cypress/e2e/search.cy.ts
+++ users-managment-app/cypress/e2e/search.cy.ts
@@ -1,10 +1,11 @@
 describe('search', () => {
+ 
   it('search', () => {
     cy.visit('/login');
     
     // Log in
-    cy.get('#username').type('ahmad');
-    cy.get('#password').type('12345');
+    cy.get('#username').type('admin3');
+    cy.get('#password').type('Admin@123');
     cy.get('button[type="submit"]').click();
     cy.url().should("include", "/");
 
@@ -16,32 +17,32 @@ describe('search', () => {
     cy.url().should("include", "/users");
 
     // Test searching for an existing user ID
-    cy.get('.search-input').type('38'); // Replace '1' with a valid user ID in your database
-    cy.get('.search-button').click();
+    cy.get('.search-input').type('38');
+    //cy.get('.search-button').click();
     cy.get('tbody').should('contain', '38'); 
 
     // Test searching for a non-existing user ID
     cy.get('.search-input').clear().type('99999'); 
-    cy.get('.search-button').click();
+    //cy.get('.search-button').click();
     cy.get('tbody').should('not.contain', '99999'); 
 
     // Test searching for an existing first name
     cy.get('.search-input').clear().type('John'); 
-    cy.get('.search-button').click();
+    //cy.get('.search-button').click();
     cy.get('tbody').should('contain', 'John'); 
     // Test searching for a non-existing first name
     cy.get('.search-input').clear().type('NonExistingName');
-    cy.get('.search-button').click();
+    //cy.get('.search-button').click();
     cy.get('tbody').should('not.contain', 'NonExistingName'); 
 
     // Test searching for an existing last name
     cy.get('.search-input').clear().type('Doe'); 
-    cy.get('.search-button').click();
+    //cy.get('.search-button').click();
     cy.get('tbody').should('contain', 'Doe'); 
 
     
     cy.get('.search-input').clear().type('NonExistingLastName');
-    cy.get('.search-button').click();
+    //cy.get('.search-button').click();
     cy.get('tbody').should('not.contain', 'NonExistingLastName'); 
 
     // Logout
